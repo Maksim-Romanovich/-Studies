92d27899a1d0206ca9b874def642b6cb293bc319
DESKTOP-J2U61QD\Дима <man.whithoutaface@mail.ru>
2022-11-23 23:46:18 +0400

    StartedTask 1.1

---
+++ Task 1/Task 1.1/Rectangl/Rectangl/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+public class RectanglArea
+{
+    public static void Main()
+    {
+        Console.Write("a: ");
+        int side_a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("b: ");
+        int side_b = Convert.ToInt32(Console.ReadLine());
+
+        int RectanglArea = (side_a * side_b);
+
+        Console.WriteLine("RectanglArea: {0}", RectanglArea);
+        Console.ReadLine();
+    }
+}
+
+
