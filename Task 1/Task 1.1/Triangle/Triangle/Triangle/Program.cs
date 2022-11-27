6cd851691d90662e9534dec0b52473f8aa68006c
DESKTOP-J2U61QD\Дима <man.whithoutaface@mail.ru>
2022-11-27 21:27:12 +0400

    StartedTask 1.1.2

---
+++ Task 1/Task 1.1/Triangle/Triangle/Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Task112
+{
+    class Task112
+    {
+        public static void Main()
+        {
+            Triangle();
+        }
+        public static void Triangle()
+        {
+            Console.WriteLine("Enter number of lines:");
+            if (int.TryParse(Console.ReadLine(), out var n) && n > 0)
+                for (var i = "*"; i.Length <= n; i += "*")
+                    Console.WriteLine(i);
+            else
+                throw new ArgumentException("Invalid argument. Use only positive numbers");
+        }
+    }
+}
